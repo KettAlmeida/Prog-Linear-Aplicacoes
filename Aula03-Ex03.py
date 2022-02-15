8c3028c35c9dff52d194932fbaa0599fc42e5ce5
Kettney Almeida dos Santos <88838899+KettAlmeida@users.noreply.github.com>
2022-02-15 18:55:48 -0300

    Delete Aula03-Ex03.py

--- Aula03-Ex03.py
+++
@@ -1,7 +0,0 @@
-#----------------------------------------------------------------------
-#Faça um programa que solicite o nome do usuário e imprima-o na vertical.
-#-----------------------------------------------------------------------
-nome = input('Digite seu nome: ')
-
-for c in nome:
-    print(c)
